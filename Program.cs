777f58d6a870255434eb9cd7ab5c6dea83a17f8c
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-01 09:40:13 -0400

    In Program class, delete static IWebHostBuilder CreateWebHostBuilder() helper method called in Main() method. Instead, have main call its return value - WebHost.CreateDefaultBuilder(args).UseStartup<Startup>() - directly. Append Build() and Run() methods to it.

--- Program.cs
+++ Program.cs
@@ -14,11 +14,8 @@ namespace PersonalWebsite
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().Build().Run();
+        }
     }
 }
