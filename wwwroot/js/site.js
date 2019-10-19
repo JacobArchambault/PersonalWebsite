73f75b285ff4865c5055b66b24db72a008fc68cf
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-18 21:13:57 -0400

    Add slide toggle for background images. Re-add local backups for bootstrap and jQuery.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿//$(".bg-image").hide().fadeIn(4000);
+﻿$("#bg-image").hide().fadeIn(4000);
+
+$("header img").click(function () {
+    $("#bg-image").slideToggle();
+    $("#bg-image2").slideToggle();
+});
