5054f79f453bbf5048221d2b47cfd5783f4d437f
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-21 15:38:34 -0400

    Convert profile and pics classes to IDs.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,16 +1,27 @@
-﻿// Cycle through background images with a fade, switching images every six seconds.
-$('.pics img:gt(0)').hide();
-setInterval(function () {
-    $('.pics :first-child').fadeOut()
-        .next('img').fadeIn()
-        .end().appendTo('.pics');
-},
-    6000);
+﻿//#region 1. Function declarations
 
 // Toggle profile photo with fade on click.
-$('.profile img:gt(0)').hide();
-$('.profile').click(function () {
-    $('.profile :first-child').fadeOut()
-        .next('img').fadeIn()
-        .end().appendTo('.profile');
-});
+function fadeCarousel(className ,eventTrigger) {
+    $('#profile').click(function () {
+        $('#profile :first-child').fadeOut()
+            .next('img').fadeIn()
+            .end().appendTo('#profile');
+    });
+}
+
+// Cycle through background images with a fade, switching images every six seconds.
+function headerFadeCarousel() {
+    setInterval(function () {
+        $('.#pics :first-child').fadeOut()
+            .next('img').fadeIn()
+            .end().appendTo('#pics');
+    }, 6000);
+}
+
+//#endregion
+
+$('#pics img:gt(0)').hide();
+$('#profile img:gt(0)').hide();
+
+headerFadeCarousel();
+fadeCarousel();
