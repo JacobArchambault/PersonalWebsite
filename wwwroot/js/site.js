60f66d4cf5613e9b8a08491a83e1a8131adbd0bd
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-21 14:00:39 -0400

    Add cycle through profile pics with fade on click. Consolidate css for specific cycled images into the img selector

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,16 @@
-﻿$('.pics img:gt(0)').hide();
+﻿// Cycle through background images with a fade, switching images every six seconds.
+$('.pics img:gt(0)').hide();
 setInterval(function () {
     $('.pics :first-child').fadeOut()
         .next('img').fadeIn()
         .end().appendTo('.pics');
 },
-    4000);
+    6000);
+
+// Toggle profile photo with fade on click.
+$('.profile img:gt(0)').hide();
+$('.profile').click(function () {
+    $('.profile :first-child').fadeOut()
+        .next('img').fadeIn()
+        .end().appendTo('.profile');
+});
