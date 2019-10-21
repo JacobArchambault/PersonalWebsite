97d2e1ff1a2ec63e255da78aad6ec3860cc2b25a
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-21 15:56:32 -0400

    Delete references to images from fadeCarousel and headerFadeCarousel functions. Functions now act more broadly on elements within .profile and .pics class, respectively.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,8 +3,8 @@
 // Toggle profile photo with fade on click.
 function fadeCarousel() {
     $('.profile').click(function () {
-        $('.profile :first-child').fadeOut()
-            .next('img').fadeIn()
+        $('.profile > :first-child').fadeOut()
+            .next().fadeIn()
             .end().appendTo('.profile');
     });
 }
@@ -12,16 +12,16 @@ function fadeCarousel() {
 // Cycle through background images with a fade, switching images every six seconds.
 function headerFadeCarousel() {
     setInterval(function () {
-        $('.pics :first-child').fadeOut()
-            .next('img').fadeIn()
+        $('.pics > :first-child').fadeOut()
+            .next().fadeIn()
             .end().appendTo('.pics');
     }, 6000);
 }
 
 //#endregion
 
-$('.pics img:gt(0)').hide();
-$('.profile img:gt(0)').hide();
+$('.pics > :gt(0)').hide();
+$('.profile > :gt(0)').hide();
 
 headerFadeCarousel();
 fadeCarousel();
