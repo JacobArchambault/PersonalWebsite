cc6f55c792ec8f670929e5579813704ea81c5211
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-25 17:37:49 -0400

    Add send_to attribute to handleDocumentLinkClicks  Google Analytics function in site.js.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,6 +11,7 @@
     function handleDocumentLinkClicks(url) {
         gtag('event', 'click',
         {
+            'send_to': 'UA-150909892-1',
             'event_category': 'Documents',
             'event_label': url,
         });
