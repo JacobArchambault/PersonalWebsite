735a4d5c7837a7fb5ee29f5d5cbf53b66d877626
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-25 20:48:47 -0400

    Remove comma after 'url' in handleDocumentLinkClicks function in site.js file.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -12,7 +12,7 @@
         gtag('event', 'click', {
             'send_to': 'UA-150909892-1',
             'event_category': 'Documents',
-            'event_label': url,
+            'event_label': url
         });
     }
 //#endregion
