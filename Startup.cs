ea803b9c91321dc937bf24cf2e91cdf08a070b1c
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-04 21:32:29 -0500

    Update application to .net 5.

--- Startup.cs
+++ Startup.cs
@@ -9,19 +9,19 @@ namespace PersonalWebsite
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services) => services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseExceptionHandler("/Error");
             app.UseHsts();
-
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
             app.UseStatusCodePagesWithRedirects("/Error/{0}");
-
-            app.UseMvc(routes => routes.MapRoute(name: "OnlyAction", template: "{action}/{id?}", defaults: new { controller = "Home", action = "Index" }));
+            app.UseRouting();
+            app.UseEndpoints(routes => routes.MapControllerRoute(name: "OnlyAction", pattern: "{action}/{id?}", defaults: new { controller = "Home", action = "Index" }));
         }
     }
 }
