8ab0d7554a9438e1bf1dbff1c2f980567c6c00f3
Jacob Archambault <jacobarchambault@gmail.com>
2024-02-21 18:01:30 -0500

    Remove obsolete SetCompatibilityVersion extension method call from Startup

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ namespace PersonalWebsite
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services) => services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc();
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
