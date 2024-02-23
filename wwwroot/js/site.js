5b7d037f399c448c6a3c70a0e7bc07cb2a643852
Jacob Archambault <jacobarchambault@gmail.com>
2024-02-23 09:18:50 -0500

    Remove extra pics and carousel to improve page load

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,4 @@
-﻿//#region 1 Function declarations
-
-// Cycle through items with fade effect.
-function fadeCarousel(containerClass) {
-    $(containerClass + '> :first-child').fadeOut()
-        .next().fadeIn()
-        .end().appendTo(containerClass);
-}
-
-// For each click on my resume or CV, send a record of it to Google Analytics.
+﻿// For each click on my resume or CV, send a record of it to Google Analytics.
 function handleDocumentLinkClicks(url) {
     gtag('event', 'click', {
         'send_to': 'UA-150909892-1',
@@ -15,15 +6,3 @@ function handleDocumentLinkClicks(url) {
         'event_label': url
     });
 }
-//#endregion
-
-// #region 2 Function calls
-
-// Hide all but the first element in my image container classes on startup.
-$('.profile > :gt(0)').hide();
-$('.pics > :gt(0)').hide();
-
-// Cycle through background images with a fade, switching images every six seconds.
-setInterval(() => { fadeCarousel(".pics"); }, 6000);
-
-// #endregion
