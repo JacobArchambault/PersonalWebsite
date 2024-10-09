9e4c418b0bfb1dd63a2d40c724a3f24137530a39
Jacob Archambault <jacobarchambault@gmail.com>
2024-10-09 15:13:08 -0400

    Remove local JS and google analytics

--- wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿// For each click on my resume or CV, send a record of it to Google Analytics.
-function handleDocumentLinkClicks(url) {
-    gtag('event', 'click', {
-        'send_to': 'UA-150909892-1',
-        'event_category': 'Documents',
-        'event_label': url
-    });
-}
